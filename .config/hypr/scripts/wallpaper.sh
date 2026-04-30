fb137d9326696714377137a5e250a40bd19d32a1
MateoGonzalezLourido <mlourido1467@gmail.com>
2026-04-30 06:20:12 -0400

    arreglado script fondo pantalla

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-#script para poner fondo de pantalla auto y aleatorio
 WALLPAPER_DIR="$HOME/Wallpapers"
 WALLPAPER=$(ls "$WALLPAPER_DIR"/*.{jpg,png} 2>/dev/null | shuf -n 1)
 
-sleep 0.5  # esperar a que el daemon esté listo (para que se vea la animacion)
-
+sleep 0.5
 swww img "$WALLPAPER" \
     --transition-type random \
     --transition-duration 2 \
     --transition-fps 60 \
     --transition-step 90
-    --transition-pos center
